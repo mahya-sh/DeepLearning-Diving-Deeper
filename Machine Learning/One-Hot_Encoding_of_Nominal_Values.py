f795bee4d2d7b93f4049a566e7297ac798753eff
Mahya Shahshahani <110742260+mahya-sh@users.noreply.github.com>
2024-08-10 23:23:47 +0330

    Create One-Hot_Encoding_of_Nominal_Values.py

---
+++ Machine Learning/One-Hot_Encoding_of_Nominal_Values.py
@@ -0,0 +1,7 @@
+import numpy as np
+def to_categorical(x, n_col=None):
+    if not n_col:
+      n_col = max(x) + 1
+    one_hot = np.zeros((x.shape[0], n_col))
+    one_hot[np.arange(x.shape[0]), x] = 1
+    return one_hot
